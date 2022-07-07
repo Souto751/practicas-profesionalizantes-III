110da705984a2ff37670c74162d6eb7fdca71a83
Agustín Souto <69278386+Souto751@users.noreply.github.com>
2022-07-07 18:49:43 -0300

    Add files via upload

---
+++ single-page-application/calculadora-v6/index.js
@@ -0,0 +1,15 @@
+import CalculatorModel from './calculatorModel.js';
+import CalculatorView from './calculatorView.js';
+
+let calcModel = new CalculatorModel();
+let calc = new CalculatorView(calcModel);
+
+document.getElementById("showModal").addEventListener("click", () => {
+    document.getElementById("modal-screen").classList.remove("hide");
+    document.getElementById("modal-content").appendChild(calc);
+});
+
+document.getElementById("close-modal").addEventListener("click", () => {
+    document.getElementById("modal-screen").classList.add("hide");
+    document.getElementById("modal-content").innerHTML = "";
+});
